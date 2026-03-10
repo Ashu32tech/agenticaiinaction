0a9a04dc103656a5d9998520fb754179e968a66c
Ashu32tech <33239769+Ashu32tech@users.noreply.github.com>
2026-03-10 05:33:37 +0530

    Create sync-call.py

---
+++ sync-call.py
@@ -0,0 +1,53 @@
+# Two tasks running in sychronous mode
+
+import time
+from agents import Agent, Runner
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+# Define two agents with different instructions
+upi_agent = Agent(
+    name="UPI_Essay_Writer",
+    instructions="Write a clear, well-structured essay in 4 paragraphs about UPI."
+)
+
+agent_api_agent = Agent(
+    name="AgentAPI_Note_Writer",
+    instructions="Write a detailed one-page note explaining the OpenAI Agents API: what it is, how it works, and its use cases."
+)
+
+ai_agent = Agent(
+    name="ai_Agent",
+    instructions="Write a concise summary of the latest developments in the field of artificial intelligence, focusing on research breakthroughs and industry applications."
+)
+
+start = time.time()
+
+# Run them sequentially (synchronous execution)
+print("Running UPI Essay agent...")
+t1 = time.time()
+result1 = Runner.run_sync(upi_agent, "Write the essay now.")
+print(f"UPI Essay completed in {time.time() - t1:.2f} seconds\n")
+
+print("Running Agent API Note agent...")
+t2 = time.time()
+result2 = Runner.run_sync(agent_api_agent, "Write the note now.")
+print(f"Agent API Note completed in {time.time() - t2:.2f} seconds\n")
+
+print("Running AI Agent  Note agent...")
+t3 = time.time()
+result3 = Runner.run_sync(ai_agent, "Write the note now.")
+print(f"Ai Agent Note completed in {time.time() - t3:.2f} seconds\n")
+
+
+print("=== Essay on Cross Border Payments ===\n")
+print(result1.final_output)
+
+print("\n=== Note on Agent API ===\n")
+print(result2.final_output)
+
+print("\n=== Note on AI Agent ===\n")
+print(result3.final_output)
+
+print(f"\nTotal execution time: {time.time() - start:.2f} seconds")
